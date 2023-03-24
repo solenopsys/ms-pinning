0abd50a49ea3711009460454e47abbbad0e99353
AlexStorm <info@alexstorm.ru>
2023-03-24 22:11:38 +0530

    install changes

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+build_push(){
+  cd exim
+  nerdctl build --platform=${ARCHS} --output type=image,name=${REGISTRY}/${NAME}:latest,push=true .
+}
+
+helm_build_push(){
+  FN=${NAME}-${VER}.tgz
+  rm ${FN}
+  helm package ./install --version ${VER}
+  curl --data-binary "@${FN}" http://helm.solenopsys.org/api/charts
+}
+
+REGISTRY=registry.solenopsys.org
+NAME=platform-ms-pinning
+ARCHS="amd64"
+VER=0.1.0
+
+
+build_push
+helm_build_push
+
+
+
+
+
