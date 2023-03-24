eebc85b80cc9e85cd633804f26b7a9f036158edb
AlexStorm <info@alexstorm.ru>
2023-03-24 10:24:59 +0530

    config

--- migrations/create_pins_table.sql
+++ migrations/create_pins_table.sql
@@ -1,6 +1,7 @@
-CREATE TABLE users (
+CREATE TABLE pins (
                        id  PRIMARY KEY,
                        user_id foreign key REFERENCES users(id),
                        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-                       pin_size bigint,
+                       size bigint,
+                       state VARCHAR(255) NOT NULL DEFAULT 'new'
 );
